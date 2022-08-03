8b85655f1eea7a124061158ebf02fdff8337d734
GerardoFilho <gerardo_filho95@hotmail.com>
2022-08-03 11:32:36 -0300

    ajustes no mail

--- mail.php
+++ mail.php
@@ -33,28 +33,20 @@
 
 <body>
 
-    <header id="top">
-        <div class="header-content header-container">
-            <div>
-                <a href="">
-                    <img src="assets/src/media/images/logoColor.png" alt="EJ Consultoria Logo">
-                </a>
-            </div>
+    <!-- Start Whatsapp Button-->
+    <a class="whatsapp mobile" href="https://wa.me/5588989898989" target="_blank">
+        <i class="fa fa-whatsapp" aria-hidden="true"></i>
+    </a>
+    <a class="whatsapp desktop" href="https://web.whatsapp.com/send?phone=5588996710713" target="_blank">
+        <i class="fa fa-whatsapp" aria-hidden="true"></i>
+    </a>
+    <!-- End Whatsapp Button-->
 
-            <div class="header-info">
-                <a href="#map">
-                    <i class="fa fa-map-marker fa-3x" aria-hidden="true"></i>
-                </a>
-
-                <span class="pipe">|</span>
-                <p>Fone/Fax (88) 9 9734-6789
-                    <br> Morada Nova - Ceará
-                    <br> Rua Damasceno Girão, nº 25, Centro
-                </p>
-
-            </div>
-        </div>
-    </header>
+    <div id="logo" class="logo-content">
+        <a href="#top">
+            <img src="assets/dist/media/images/logo.png" alt="EJ Consultoria Logo">
+        </a>
+    </div>
 
     <nav id="nav-menu">
         <div class="nav-icon-container">
@@ -67,37 +59,30 @@
                 <span></span>
             </div>
         </div>
-        <ul class="nav-content nav-container">
-            <li>
-                <a href="#top" class="hvr-bounce-to-top">Início</a>
-            </li>
-            <li>
-                <a href="#about" class="hvr-bounce-to-top">Quem Somos</a>
-            </li>
-            <li>
-                <a href="#service-box" class="hvr-bounce-to-top">Nossos Serviços</a>
-            </li>
-            <li>
-                <a href="engenharia.html" class="hvr-bounce-to-top">Projetos de Engenharia</a>
-            </li>
-            <li>
-                <a href="clientes.html" class="hvr-bounce-to-top">Clientes</a>
-            </li>
-            <li>
-                <a href="juridica.html" class="hvr-bounce-to-top">Segurança Jurídica</a>
-            </li>
-            <li>
-                <a href="sustentabilidade.html" class="hvr-bounce-to-top">Sustentabilidade</a>
-            </li>
-            <li>
-                <a href="#contact" class="hvr-bounce-to-top">Contato</a>
-            </li>
-        </ul>
+        <div class="navbarContainer">
+            <ul class="nav-content nav-container">
+                <li>
+                    <a href="#top" class="hvr-bounce-to-top">Início</a>
+                </li>
+                <li>
+                    <a href="#about" class="hvr-bounce-to-top">Quem Somos</a>
+                </li>
+                <li>
+                    <a href="#service-box" class="hvr-bounce-to-top">Nossos Serviços</a>
+                </li>
+                <li>
+                    <a href="#clientes" class="hvr-bounce-to-top">Nossos Clientes</a>
+                </li>
+                <li>
+                    <a href="#contact" class="hvr-bounce-to-top">Contato</a>
+                </li>
+            </ul>
+        </div>
     </nav>
 
 <?php
 
-   $to         = 'ej.engenhariajuridica@gmail.com';
+   $to         = 'comercial@ejconsultoria.com';
    $subject    = $_POST['subject'];
    $text       = $_POST['message'];
    $username   = $_POST['nome'];
@@ -158,51 +143,38 @@
                 <h5>"O que parece uma simples consulta, o tornará mais competitivo."</h5>
 
                 <input class="input" type="text" name="nome" id="nome" placeholder="Nome completo">
-                <input class="input" type="text" name="nome_empresa" id="nome_empresa" placeholder="Nome de sua empresa">
+                <input class="input" type="text" name="nome_empresa" id="nome_empresa"
+                    placeholder="Nome de sua empresa">
                 <input class="input" type="email" name="email" id="email" placeholder="Seu email">
                 <input class="input" type="text" name="subject" id="subject" placeholder="Assunto da mensagem">
-                <textarea class="input" name="message" id="message" rows="3" placeholder="Digite aqui sua mensagem..."></textarea>
+                <textarea class="input" name="message" id="message" rows="3"
+                    placeholder="Digite aqui sua mensagem..."></textarea>
 
                 <button class="btn-secondary left larg" type="submit">Enviar</button>
             </form>
         </div>
     </section>
 
-    <section id="map">
-        <iframe id="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d608.7762352143369!2d-38.37354007121604!3d-5.108446572814159!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7bbe6cf620f291f%3A0xdb376d3724ae4428!2sR.+Damasceno+Gir%C3%A3o%2C+Morada+Nova+-+CE%2C+62940-000!5e0!3m2!1spt-BR!2sbr!4v1507342105725"
-            width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-    </section>
-
     <footer>
         <div class="footer-content">
 
             <div class="address">
-                <p>Rua Damasceno Girão, nº25, Sala B, Centro
-                    <br> Morada Nova - Ceará, CEP: 629400-000
-                    <br>
-                    <a href="tel:88997346789">
-                        <i class="fa fa-whatsapp" aria-hidden="true"></i> (88) 9 97346789
-                        <br>
-                        <a href="mailto:ej.engenhariajuridica@gmail.com">
-                            <i class="fa fa-envelope-o" aria-hidden="true"></i> ej.engenhariajuridica@gmail.com</a>
-                </p>
-            </div>
 
-            <span class="pipe">|</span>
-
-            <div class="address">
-                <p>SGAN, nº 220, Módulo C, Bloco B, Asa Norte
-                    <br> Brasília - Distrito Federal, CEP: 70790-123
+                <a href="tel:88996710713">
+                    <i class="fa fa-whatsapp" aria-hidden="true"></i> (88) 9 96710713
                     <br>
-                    <a href="tel:88999004005">Fone/Fax: (88) 9 99004005</a>
+                    <a href="https://www.instagram.com/ejconsultoriaoficial/" target="_blank"><i class="fa fa-instagram"
+                            aria-hidden="true"></i> ejconsultoriaoficial </a>
                     <br>
-
-                </p>
+                    <a href="mailto:comercial@ejconsultoria.com">
+                        <i class="fa fa-envelope-o" aria-hidden="true"></i> comercial@ejconsultoria.com</a>
+                    </p>
+                    <span id="cnpj">CNPJ: 22.212.407/0001-15</span>
             </div>
 
         </div>
 
-        <span id="cnpj">CNPJ: 22.212.407/0001-15</span>
+        <p><span></span></p>
 
     </footer>
 
